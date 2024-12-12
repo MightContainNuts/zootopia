5aaf7915afe31fb192b416d2c74fddfb2fa01f86
MightContainNuts <mightcontainnuts@icloud.com>
2024-12-12 13:30:41 +0100

    added logging and pytest

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,14 +1,47 @@
 import json
+from json import JSONDecodeError
+
+import logging
+import os
 
 FoxDict = list[dict]
 
-
-def load_data(json_file) -> FoxDict:
-    with open(json_file, "r") as handler:
-        return json.load(handler)
+json_file = "animals_data.json"
 
 
-def step_1(fox_dict: FoxDict) -> FoxDict:
+def setup_logging() -> logging:
+    folder = "logging"
+    file_name = "logs.txt"
+    path = os.path.join(folder, file_name)
+    os.makedirs(folder, exist_ok=True)
+    logging.basicConfig(
+        filename=path,
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(message)s",
+    )
+    logger = logging.getLogger(__name__)
+    logger.info("logging started...")
+    return logger
+
+
+def load_data(json_file, logger: logging) -> FoxDict:
+    try:
+        logger.info("loading data from %s", json_file)
+        with open(json_file, "r") as handler:
+            if handler:
+                logger.info(f"{json_file} loaded")
+                return json.load(handler)
+            else:
+                logger.error(f"{json_file} empty")
+    except FileNotFoundError:
+        logger.error(f"{json_file} not found")
+    except JSONDecodeError:
+        logger.error(f"{json_file} could not be decoded")
+    except Exception as e:
+        logger.error("Unexpected error caught %s", e)
+
+
+def step_1(fox_dict: FoxDict, logger: logging) -> FoxDict:
     """
     get the fox_dict and get specific fields from it.
     diet, location, type and name.
@@ -17,45 +50,55 @@ def step_1(fox_dict: FoxDict) -> FoxDict:
     :type foxdict:
     :return:
     """
-
+    logger.info("Starting to sort Foxes as per Step1")
     sorted_foxs = []
+    if fox_dict:
+        logging.info("fox_dict found starting to parse")
+        for item in fox_dict:
+            name = item.get("name", "")
+            characteristics = item.get("characteristics", {})
+            diet = characteristics.get("diet", "")
+            location = item.get("locations", [])
+            fox_type = characteristics.get("type", "")
 
-    for item in fox_dict:
-        name = item.get("name", "")
-        characteristics = item.get("characteristics", {})
-        diet = characteristics.get("diet", "")
-        location = item.get("locations", [])
-        fox_type = characteristics.get("type", "")
-
-        details = {
-            "Name": name,
-            "Diet": diet,
-            "Location": location[0],
-            "Type": fox_type,
-        }
-        sorted_foxs.append({key: value for key, value in details.items() if value})
-
+            details = {
+                "Name": name,
+                "Diet": diet,
+                "Location": location[0] if location else None,
+                "Type": fox_type,
+            }
+            sorted_foxs.append({key: value for key, value in details.items() if value})
+            logger.info(f"Fox added to list with {details}")
+    else:
+        logging.error("fox_dict is empty")
     return sorted_foxs
 
 
-def print_sorted_fox(sorted_foxs: list[dict]) -> None:
+def print_sorted_fox(sorted_foxs: list[dict], logger: logging) -> None:
     """
     :param sorted_foxs:
     :type sorted_foxs:
     :return:
     :rtype:
     """
-    for fox in sorted_foxs:
-        for key, value in fox.items():
-            print(f"{key} : {value}")
-        print("\n")
+    logger.info("Starting to print sorted foxs to display")
+    if sorted_foxs:
+        logger.info("sorted_foxs found")
+        for fox in sorted_foxs:
+            for key, value in fox.items():
+                print(f"{key} : {value}")
+            print()
+        else:
+            logger.error("sorted foxs is empty")
+    logger.info(("finished printing sorted_foxes"))
 
 
 def main() -> None:
-    json_file = "animals_data.json"
-    fox_dict = load_data(json_file)
-    ans_task1 = step_1(fox_dict)
-    print_sorted_fox(ans_task1)
+    logger = setup_logging()
+    logger.info("animal_web_generator started")
+    fox_dict = load_data(json_file, logger)
+    ans_task1 = step_1(fox_dict, logger)
+    print_sorted_fox(ans_task1, logger)
 
 
 if __name__ == "__main__":
--- tests/test_animal_web_generator.py
+++ tests/test_animal_web_generator.py
@@ -1,2 +1,95 @@
+import logging
+import pytest
+from animals_web_generator import setup_logging, load_data, step_1, print_sorted_fox
+import os
+
+
 def test_dummy():
     pass
+
+
+@pytest.fixture(scope="module")
+def logger():
+    logger = setup_logging()
+    yield logger
+    logging.shutdown()
+
+
+def test_logging(caplog, logger):
+    folder = "logging"
+    file_name = "logs.txt"
+    path = os.path.join(folder, file_name)
+    with caplog.at_level(logging.INFO):
+        setup_logging()
+    assert os.path.exists(path), f"File {path} does not exist"
+    assert "logging started..." in caplog.text, "logging not started"
+
+
+def test_load_json(caplog, logger):
+    folder = ".."
+    json_file = "animals_data.json"
+    os.path.join(folder, json_file)
+    with caplog.at_level(logging.INFO):
+        fox_data = load_data(json_file, logger)
+    assert f"{json_file} loaded" in caplog.text
+    assert fox_data, "No data in JSON file"
+
+
+def test_sort_fox_1(caplog, logger):
+    test_fox_dict_1 = [
+        {
+            "name": "Arctic Fox",
+            "locations": ["Eurasia"],
+            "characteristics": {
+                "type": "Furry Fox",
+                "diet": "Carnivore",
+            },
+        },
+    ]
+    with caplog.at_level(logging.INFO):
+        sorted_fox = step_1(test_fox_dict_1, logger)
+    for fox in sorted_fox:
+        assert fox["Name"] == "Arctic Fox"
+        assert fox["Location"] == "Eurasia"
+        assert fox["Type"] == "Furry Fox"
+        assert fox["Diet"] == "Carnivore"
+
+
+def test_sort_fox_2(caplog, logger):
+    test_fox_dict_2 = [
+        {"name": "Missing Data Fox", "locations": [], "characteristics": {}},
+    ]
+    with caplog.at_level(logging.INFO):
+        sorted_fox = step_1(test_fox_dict_2, logger)
+    for fox in sorted_fox:
+        assert fox["Name"] == "Missing Data Fox"
+        assert "Location" not in fox
+        assert "Type" not in fox
+        assert "Diet" not in fox
+
+
+def test_print_sorted_fox(capfd, caplog, logger):
+    test_fox_dict = [
+        {
+            "Name": "Arctic Fox",
+            "Diet": "Carnivore",
+            "Location": "Eurasia",
+            "Type": "Furry Fox",
+        },
+        {"Name": "Missing Data Fox"},
+    ]
+
+    with caplog.at_level(logging.INFO):
+        print_sorted_fox(test_fox_dict, logger)
+
+    assert "Starting to print sorted foxs to display" in caplog.text
+    assert "sorted_foxs found" in caplog.text
+    assert "finished printing sorted_foxes" in caplog.text
+
+    captured = capfd.readouterr()
+    expected_output = (
+        "Name : Arctic Fox\nDiet : Carnivore\nLocation : Eurasia\nType : Furry Fox\n\n"
+        "Name : Missing Data Fox\n\n"
+    )
+
+    assert captured.out == expected_output
